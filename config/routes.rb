53af115d6b6279dc4fd4aa4fb88968604b2e3575
hddang2 <huydang520@yahoo.com>
2022-09-19 16:24:47 -0500

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   root 'home#index'
-  resources :books
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
